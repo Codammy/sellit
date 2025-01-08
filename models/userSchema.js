e2427fac29350beb9ab1a5e66fd1599aab813b7c
Mr Dami <damisco005@gmail.com>
2025-01-08 01:56:49 +0100

    updated user models by group

--- models/userSchema.js
+++ models/userSchema.js
@@ -2,30 +2,43 @@ import  {Schema, model} from "mongoose"
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
-    firstname: {type: String, required: true},
-    lastname: {type: String, required: true},
-    email: {type: String, required: true},
-    ratings: {type: Number, required: true},
-    city: String,
-    state: String,
-    address: String,
-    pic_url: String,
-    about: String,
-    phoneNo: {type: String, maxLength: 14},
-    whatsappNo: {type: String, maxLength: 14},
-    facebookUrl: String,
-    twitterUrl: String,
-    website: String,
+    email: {type: String, unique: true, required: true},
+    profile: {
+        firstname: {type: String, required: true},
+        lastname: {type: String, required: true},
+        about: String,
+        phoneNo: {type: String, maxLength: 14},
+    },
+    ratings: {type: Number, default: 0},
+    password: {type: String, required: true},
+    addr: {
+        city: String,
+        state: String,
+        address: String,
+    },
+    social: {
+        whatsappNo: {type: String, maxLength: 14},
+        pic_url: String,
+        facebookUrl: String,
+        twitterUrl: String,
+        website: String,
+    },
     emailVerified: Boolean,
     subscription: {type: String, enum: ['paid', 'free']}
 }, {timestamps: true});
 
-userSchema.pre('save', function() {
-        bcrypt.hash(this._password, 10, function(err, result){
-            if (err) throw new Error(err);
-            this._password = result;
-        })
+userSchema.pre('save', function(next) {
+    const user = this
+    bcrypt.genSalt(10, (err, result)=> {
+        if (err) return next(err)
+        bcrypt.hash(user.password, result, function(err, hash){
+        if (err) return next(err);
+            user.password = hash;
+            next()
+        });
+    })
 })
 
 const User = model('users', userSchema);
+User.syncIndexes().catch((err)=> {throw new Error(err)});
 export default User;
